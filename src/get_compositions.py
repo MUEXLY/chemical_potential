6b2a49a0166831a72e560347c0fdb8a40999ade2
Jacob Jeffries <jwjeffr@login001.palmetto.clemson.edu>
2024-03-28 12:50:10 -0400

    shuffled compositions

--- src/get_compositions.py
+++ src/get_compositions.py
@@ -1,6 +1,7 @@
 import sys
 from ast import literal_eval
 from itertools import product
+from random import shuffle
 
 
 def main():
@@ -9,7 +10,8 @@ def main():
     compositions = literal_eval(
         "".join([item for sublist in sys.argv[1:] for item in sublist])
     )
-    possible_compositions = product(*compositions)
+    possible_compositions = list(product(*compositions))
+    shuffle(possible_compositions)
 
     # only print out composition if it sums to 1
     for p in possible_compositions:
